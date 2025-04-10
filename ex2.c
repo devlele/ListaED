0a0215eb0044439c82f3f300cc28360efad87630
Alefe Freitas Santos <alefe.santos@afinz.com.br>
2025-04-10 19:00:53 -0300

    ex2

---
+++ ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXIMO 100
+
+typedef struct aluno{
+    char ra[14];
+    char codDiscip[6];
+    float nota;
+    int qtdFaltas;
+}Aluno;
+
+int main() {
+    struct lista[MAXIMO];
+    
+}
